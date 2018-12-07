56a34c0bf2d397469ea1a73295450ae18cdae690
munasinada <munasinada@gmail.com>
2018-12-06 20:33:47 -0800

    deployment

--- app2/config/routes.rb
+++ app2/config/routes.rb
@@ -1,7 +1,23 @@
+
+
 Rails.application.routes.draw do
+get '/login', to: 'access#new'
+  get 'access/new'
+  get 'access/create'
+  get 'access/destroy'
+  get 'admin/index'
+  resources :users
+  resources :orders
+  resources :lineitems
+  resources :carts
   get 'shopper/index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "shopper#index", as: 'root'
  get '/shopper', to: 'shopper#index'
+ get '/admin', to: 'admin#index'
+ post '/admin', to: 'admin#index'
+ post '/admin/index', to: 'admin#index'
+ post '/login', to: 'admin#index'
+
 end
